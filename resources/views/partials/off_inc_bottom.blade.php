8d78d1ef732e2363ada9472766b9e48c9ab3118e
Chinedu Okemiri <jokemiri@gmail.com>
2020-04-07 09:56:09 +0100

    Summernote Update

--- resources/views/partials/off_inc_bottom.blade.php
+++ resources/views/partials/off_inc_bottom.blade.php
@@ -17,8 +17,8 @@
 
 <script  type="text/javascript" src="{{ asset('assets/js/fileinput.min.js') }}"></script>
 
-<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet">
-<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.min.js"></script>
 
 
 
